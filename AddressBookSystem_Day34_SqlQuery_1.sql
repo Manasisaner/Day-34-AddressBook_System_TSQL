291f4acbc35c6f9c8600a3994a21c24774501ade
Manasisaner <102273402+Manasisaner@users.noreply.github.com>
2022-06-15 22:42:02 +0530

    Add files via upload

---
+++ AddressBookSystem_Day34_SqlQuery_1.sql
@@ -0,0 +1,33 @@
+﻿create database AddressBookSystemNew
+use AddressBookSystemNew;
+
+create table Address_Book_service
+(
+ BookId int not null Primary Key Identity,
+ BookName varchar(25) not null,
+ BookType varchar(25),
+ DateofBookCreation date
+)
+create Table Contacts
+(
+ BookId int not null Foreign Key References Books(BookId),
+ PersonId int not null Primary Key identity,
+ PersonName varchar(30) not null,
+ Gender char not null,
+ MobileNo Bigint
+)
+create Table Addresses
+(
+ PersonId int not null Foreign Key References Contacts(PersonId),
+ AddressId int not null Primary Key identity,
+ City varchar(20),
+ State varchar(20),
+ ZipCode int,
+ Country varchar(20)
+)
+
+select * from Address_Book_service
+
+drop table Books
+drop table Contacts
+drop table Addresses
